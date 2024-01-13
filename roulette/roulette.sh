c795f34cd6cdb6885fc32426052ff5503be5f7c5
junkanai <kanai.jun.1020@gmail.com>
2024-01-13 19:13:33 +0900

    hypot

--- roulette/roulette.sh
+++ roulette/roulette.sh
@@ -2,23 +2,23 @@
 #./a.out
 #g++ -std=gnu++17 -O3 roulette_float_onepoint.cpp
 #./a.out
-g++ -std=gnu++17 -O3 roulette_ternary_onepoint.cpp
-./a.out
+#g++ -std=gnu++17 -O3 roulette_ternary_onepoint.cpp
+#./a.out
 g++ -std=gnu++17 -O3 roulette_scalar_onepoint.cpp
 ./a.out
 #g++ -std=gnu++17 -O3 roulette_binary_twopoint.cpp
 #./a.out
 #g++ -std=gnu++17 -O3 roulette_float_twopoint.cpp
 #./a.out
-g++ -std=gnu++17 -O3 roulette_ternary_twopoint.cpp
-./a.out
+#g++ -std=gnu++17 -O3 roulette_ternary_twopoint.cpp
+#./a.out
 g++ -std=gnu++17 -O3 roulette_scalar_twopoint.cpp
 ./a.out
 #g++ -std=gnu++17 -O3 roulette_binary_uniform.cpp
 #./a.out
 #g++ -std=gnu++17 -O3 roulette_float_uniform.cpp
 #./a.out
-g++ -std=gnu++17 -O3 roulette_ternary_uniform.cpp
-./a.out
+#g++ -std=gnu++17 -O3 roulette_ternary_uniform.cpp
+#./a.out
 g++ -std=gnu++17 -O3 roulette_scalar_uniform.cpp
 ./a.out
